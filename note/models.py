aef58114eb674ae0ff29beb0958ff0579e0ce576
Nushrafawmy <31157791+Nushrafawmy@users.noreply.github.com>
2021-10-09 06:33:46 +0530

    Delete note directory

--- note/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Note(models.Model):
-    title = models.CharField(max_length=200, blank=False, default='')
-    text = models.TextField()
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date =models.DateTimeField(auto_now=True)
